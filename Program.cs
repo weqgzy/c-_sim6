77795edcabc168c1ecb399ec6262e90c8ce5769c
SAVAGE <proj_1>
2023-04-21 12:12:08 +0300

    all_done

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.//
+
+Console.Write("Введите числа(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+if (count <= 0)
+{
+   System.Console.WriteLine("чисел больше нуля нет"); 
+
+}
+else Console.WriteLine($"числел больше нуля: {count}");
